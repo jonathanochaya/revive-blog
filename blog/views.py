9508a74fc61995aa5bdbd423da521c848f38af79
Jonathan Ochaya <wangolojowel@gmail.com>
2023-03-17 14:38:02 +0300

    Converted post list functional view to class based view and modified pagination template to use class based view page object

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 
+from django.views.generic import ListView
+
+
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    template_name = 'blog/post/list.html'
+    context_object_name = 'posts'
+    paginate_by = 3
 
 
 def post_detail(request, year, month, day, post):
